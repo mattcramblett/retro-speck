37feebad8805626e6bfd3480a4f33d681e42d2dd
Matt Cramblett <Cramblett5@gmail.com>
2024-12-23 17:35:27 -0500

    move my explanation

--- src/components/retro/retro-card.tsx
+++ src/components/retro/retro-card.tsx
@@ -4,14 +4,13 @@ import { useRetroCards } from "@/hooks/cards/use-retro-cards";
 import { Card } from "@/types/model";
 import { useState, useEffect } from "react";
 
-// The useState/useEffect combo for `content` is to prevent the textarea cursor from jumping as a user types.
-// The useMutation update lifecycle is somehow slightly out of sync with DOM rendering, probably because it's
-// expected to be async. This just keeps the data stable with rendering cycles but also fresh.
 export function RetroCard({ retroId, initialCard }: { retroId: number, initialCard: Card }) {
   const { useUpdateCard, useCard } = useRetroCards({ retroId });
   const { data: card } = useCard({ retroId, cardId: initialCard.id, initialData: initialCard });
   const { mutate } = useUpdateCard(initialCard.id);
 
+  // The useState/useEffect combo for `content` is to prevent the textarea cursor from jumping as a user types.
+  // The useMutation update lifecycle is slightly out of sync with DOM rendering because it's expected to be async.
   const [content, setContent] = useState(initialCard.content);
   useEffect(() => setContent(card?.content || ""), [card?.content]);
 
