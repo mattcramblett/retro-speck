bfad19765165cf81c73e5eb686cf9719bd968e64
Matt Cramblett <Cramblett5@gmail.com>
2025-01-01 15:08:35 -0500

    add postgres connection timeout settings

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,14 @@
-import 'dotenv/config'
+import "dotenv/config";
 
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 const connectionString = process.env.DATABASE_URL!;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false });
+export const client = postgres(connectionString, {
+  prepare: false,
+  connect_timeout: 10,
+  idle_timeout: 300,
+});
 export const db = drizzle(client);
