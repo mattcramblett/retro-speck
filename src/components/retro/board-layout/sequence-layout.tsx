3347d3ba4c71e7c8057273685e6958557d04b65b
Matt Cramblett <Cramblett5@gmail.com>
2025-01-09 18:19:54 -0500

    add ui elements for discussion

--- src/components/retro/board-layout/sequence-layout.tsx
+++ src/components/retro/board-layout/sequence-layout.tsx
@@ -4,6 +4,9 @@ import { Card, Column, Participant, Retro } from "@/types/model";
 import { RetroTopic } from "../topic/retro-topic";
 import { RetroColumn } from "../retro-column";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useUser } from "@/hooks/auth/use-user";
+import { Button } from "@/components/ui/button";
+import { MoveLeft, MoveRight } from "lucide-react";
 
 export function SequenceLayout({
   initialRetro,
@@ -25,15 +28,33 @@ export function SequenceLayout({
       enabled: !!retro?.currentTopicId,
     },
   );
+  const { data: user } = useUser();
+  const isFacilitator = retro?.facilitatorUserId === user?.id;
+
+  const handleAdvance = () => {
+  }
+  
+  const handleReverse = () => {
+  }
 
   return (
     <div className="flex w-full items-center justify-center">
-      <div className="flex items-center">
+      <div className="flex items-center gap-4">
+        {isFacilitator && (
+          <Button onClick={handleReverse}>
+            <MoveLeft />
+          </Button>
+        )}
         {isPending && <Skeleton className="rounded-xl w-full h-48" />}
         {!isPending && (
           <RetroColumn>
             <RetroTopic retroId={retroId} topicId={topic?.id || 0} />
           </RetroColumn>
+        )}
+        {isFacilitator && (
+          <Button onClick={handleAdvance}>
+            <MoveRight />
+          </Button>
         )}
       </div>
     </div>
