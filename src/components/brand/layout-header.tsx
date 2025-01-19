518b3291c59c2394349caa7b4921e8e1d1ab2462
Matt Cramblett <Cramblett5@gmail.com>
2025-01-18 20:40:42 -0500

    title links to home

--- src/components/brand/layout-header.tsx
+++ src/components/brand/layout-header.tsx
@@ -1,14 +1,17 @@
-import { Header } from "../ui/header"
-import { Title } from "./title"
-import { Logo } from "./logo"
+import { Header } from "../ui/header";
+import { Title } from "./title";
+import { Logo } from "./logo";
+import Link from "next/link";
 
 export function LayoutHeader() {
   return (
     <Header>
       <div className="flex w-fit items-center gap-2">
         <Logo size={24} linkToHome />
-        <Title responsive={true} />
+        <Link href={"/"}>
+          <Title responsive={true} />
+        </Link>
       </div>
     </Header>
-  )
+  );
 }
